954dfb4369dacfb6eca89a7e8279366e51288914
ammar <haleesammar@gmail.com>
2019-07-11 16:48:13 +0300

    initial  commit

---
+++ server/index.js
@@ -0,0 +1,220 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const pino = require('express-pino-logger')();
+const puppeteer = require('puppeteer');
+var fs = require('fs');
+
+
+/*
+Correct Credintials:
+
+Email: zakib@erabia.com
+
+Passwrod: de4***feef516he6DD3$SA2
+*/
+
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(pino);
+
+
+
+
+async function getLogin() {
+
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+ 
+  await page.goto('https://stg.erabia.io/erabiastore/?site=yerabiab2c-ae');
+  await page.screenshot({path: 'Files/Login/yerabia.png'});
+  await page.waitFor(1000); 
+  await page.click('body > main > header > nav.navigation.navigation--middle.js-navigation--middle.padd-wide > div > div.row.desktop__nav > div.nav__left.col-xs-12.col-sm-9.col-lg-10 > div > div.col-sm-2.hidden-xs.visible-sm.mobile-menu > button');
+  await page.waitFor(1000); 
+  await page.click('body > main > header > nav.navigation.navigation--bottom.js_navigation--bottom.js-enquire-offcanvas-navigation.dark > ul > li > a');
+  await page.waitFor(1000); 
+  await page.type('.login-left-content-component #j_username', 'zakib@erabia.com');
+  await page.type('.login-left-content-component #j_password', 'de4***feef516he6DD3$SA2');
+  await page.click('.login-left-content-component #loginForm > button');
+
+  
+}
+
+
+
+
+async function getReg(user) {
+console.log(user)
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+ 
+  let userCred = {
+
+    fname:'Ammar',
+    lname:'Halees',
+    email: user === undefined ||user==="" || user ===null?`${Math.random().toString(36).substring(7)}@gmail.com` : user,
+    password: 'de4***feef516he6DD3$SA2',
+    number:'0798949000',
+  
+  }
+
+  await page.goto('https://stg.erabia.io/erabiastore/?site=yerabiab2c-ae');
+  await page.screenshot({path: 'Files/Register/yerabia.png'});
+  await page.waitFor(1000); 
+  await page.click('body > main > header > nav.navigation.navigation--middle.js-navigation--middle.padd-wide > div > div.row.desktop__nav > div.nav__left.col-xs-12.col-sm-9.col-lg-10 > div > div.col-sm-2.hidden-xs.visible-sm.mobile-menu > button');
+  await page.waitFor(1000); 
+  await page.click('body > main > header > nav.navigation.navigation--bottom.js_navigation--bottom.js-enquire-offcanvas-navigation.dark > ul > li > a');
+  await page.waitFor(1000); 
+  page.hover(".user-register__headline")
+  await page.screenshot({path: 'Files/Register/register.png'});
+
+  await page.type('#firstName', userCred.fname);
+  await page.type('#lastName', userCred.lname);
+
+  await page.waitFor(1000); 
+
+  await page.click('.row > .col-md-4 > div >div > div > button')
+  await  page.keyboard.press('ArrowDown');
+  await   page.keyboard.press('Enter');
+
+
+  await page.click('#registerForm > div:nth-child(5) > div > div > div > div > button')
+  await  page.keyboard.press('ArrowDown');
+  await   page.keyboard.press('Enter');
+
+  
+
+  
+
+  await page.click('#registerForm > div:nth-child(6) > div > div > div > div > button')
+  await  page.keyboard.press('ArrowDown');
+  await   page.keyboard.press('Enter');
+
+
+  await page.click('#registerForm > div:nth-child(7) > div > div > div.btn-group.bootstrap-select.day.form-control > button')
+  await  page.keyboard.press('ArrowDown');
+  await   page.keyboard.press('Enter');
+
+
+  await page.click('#registerForm > div:nth-child(7) > div > div > div.btn-group.bootstrap-select.month.form-control > button')
+  await  page.keyboard.press('ArrowDown');
+  await   page.keyboard.press('Enter');
+
+
+  await page.click('#registerForm > div:nth-child(7) > div > div > div.btn-group.bootstrap-select.year.form-control > button')
+  await  page.keyboard.press('ArrowDown');
+  await   page.keyboard.press('Enter');
+
+  await page.click('#registerForm > div:nth-child(8) > div > div > div > div > button')
+  await  page.keyboard.press('ArrowDown');
+  await   page.keyboard.press('Enter');
+
+
+  await page.click('#registerForm > div:nth-child(9) > div > div > div > div > button')
+  await  page.keyboard.press('ArrowDown');
+  await   page.keyboard.press('Enter');
+
+
+  await page.click('#registerForm > div:nth-child(10) > div.col-md-4.col-sm-5.col-xs-12 > div > div.control > div > button')
+  await  page.keyboard.press('ArrowDown');
+  await   page.keyboard.press('Enter');
+
+
+  await page.type('#email',userCred.email);
+  await page.type('#password', userCred.password);
+  await page.type('#checkPwd', userCred.password);
+  
+  await page.type('#mobileNumber', userCred.number);
+
+
+  
+
+  await page.waitFor(1000); 
+
+  await page.setViewport({ width: 1920, height: 1080 });
+
+  await page.click('#termsandconditions')
+
+  await page.click('#registerForm > div:nth-child(16) > div > div.form-actions.clearfix > button')
+
+  
+
+
+  await page.waitFor(3000); 
+  await page.screenshot({path: 'Files/Register/registerSuccessorFailure.png'});
+
+
+  const result = await page.evaluate(() => {
+
+    let myinfo = document.querySelector('body > main > div.padd-wide.content-page-design > div.global-alerts > div').innerText // Select all Products
+
+
+    return myinfo; // Return our data array 
+
+  })
+
+  return result;
+
+
+ }
+ 
+
+app.get('/api/greeting', (req, res) => {
+ 
+
+
+  res.setHeader('Content-Type', 'application/json');
+ 
+    if(req.query.name ==="reg"){
+
+      getReg(req.query.user).then((value) => {
+        console.log(value); // Success!
+      
+        fs.writeFile('Files/Register/newfile.txt', JSON.stringify(value), function (err) {
+          if (err) throw err;
+          console.log('File is created successfully.');
+        }); 
+        
+      
+      
+      }).then(()=>console.log("file created"));
+
+    }
+
+    else if(req.query.name ==="login"){
+
+
+      getLogin()
+      // getLogin().then((value) => {
+      //   console.log(value); // Success!
+      
+      //   fs.writeFile('newfile.txt', JSON.stringify(value), function (err) {
+      //     if (err) throw err;
+      //     console.log('File is created successfully.');
+      //   }); 
+        
+      
+      
+      // });
+    }
+
+
+
+
+res.send(JSON.stringify({ greeting: "hi" }));
+
+
+
+
+});
+
+
+
+
+
+
+
+
+app.listen(3002, () =>
+  console.log('Express server is running on localhost:3002')
+);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,116 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      greeting: ''
+    };
+    
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleregEmail = this.handleregEmail.bind(this);
+    this.handleloginEmail = this.handleloginEmail.bind(this);
+
+  }
+
+
+  handleSubmit(page,user) {
+    fetch(`/api/greeting?name=${encodeURIComponent(page)}&user=${encodeURIComponent(user)}`)
+      .then(response => response.json())
+      .then(res => this.setState({
+
+        greeting:res
+
+      }
+    
+    ,()=>console.log(this.state.greeting.greeting)
+      ) );
+  }
+
+
+  handleregEmail(event){
+event.preventDefault();
+    this.setState({
+
+      regEmail:event.target.value
+
+    },()=>console.log(this.state.regEmail))
+
+  }
+
+
+  handleloginEmail(event){
+    event.preventDefault();
+        this.setState({
+    
+          loginEmail:event.target.value
+    
+        },()=>console.log(this.state.loginEmail))
+    
+      }
+
+  render() {
+
+    let r = Math.random().toString(36).substring(7);
+console.log(r);
+    return (
+      <div className="App">
+     
+
+      <h1 className="myheader">Erabia Page Crawler (Automated Test)</h1>
+
+     <div className="mycontainer">
+
+      <div className="listItem">
+        
+          <h3  className="myheader"> Register Page</h3>
+
+          <p style={{color:"white",fontStyle:"italic"}}>Note: If no email is provided; a random one will be generated!</p>
+
+          <form>
+
+            <input className="inputs_x" onChange={this.handleregEmail}  value={this.state.regEmail} placeholder="email"/>
+
+          </form>
+
+          <button style={{padding:"10px",borderRadius:"10px"}} onClick={()=>this.handleSubmit("reg",this.state.regEmail)}>Rgister Page</button>
+
+      </div>
+       
+      <div className="listItem">
+      <h3  className="myheader"> Login Page</h3>
+
+      <p style={{color:"white",fontStyle:"italic"}}>Note: If no email is provided; a default one will be used!</p>
+
+  <form>
+
+  <input className="inputs_x" onChange={this.handleloginEmail}  value={this.state.loginEmail} placeholder="email"/>
+
+  </form>
+
+
+      <button style={{padding:"10px",borderRadius:"10px"}} onClick={()=>this.handleSubmit("login",this.state.loginEmail)}>Login Page</button>
+
+      </div>
+
+      <div className="listItem">
+      <h3  className="myheader"> -- Page</h3>
+      <button style={{padding:"10px",borderRadius:"10px"}} onClick={()=>this.handleSubmit("--")}>-- Page</button>
+
+      </div>
+  
+
+     </div>
+
+      </div>
+    );
+  }
+
 }
+
+
 
 export default App;
